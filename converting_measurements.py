f9412cc1c7695ee7e534d394c1ecf5c59c39853c
Michele Reynolds <xandermansmom@gmail.com>
2023-07-19 19:55:19 -0400

    SNHU Introduction to Scripting

--- converting_measurements.py
+++ converting_measurements.py
@@ -1,7 +1,12 @@
+# define function print_total_inches
 def print_total_inches(num_feet, num_inches):
-    print('Total inches:',  num_feet * 12 +  num_inches)
+    # print statement is the sum of feet times 12 plus the number of inches
+    print('Total inches:',  num_feet * 12 + num_inches)
 
 
+# input feet and inches
 feet = int(input())
 inches = int(input())
+
+# print defined function with parameters feet and inches
 print_total_inches(feet, inches)
