52c1fa4d62f9a4e72712d58542059248713befff
Michele Reynolds <xandermansmom@gmail.com>
2023-07-19 21:36:58 -0400

    SNHU Introduction to Scripting

---
+++ hierarchical_function_calls.py
@@ -0,0 +1,17 @@
+def calc_circle_area(circle_diameter):
+    pi_val = 3.14159265
+
+    circle_radius = circle_diameter / 2.0
+    circle_area = pi_val * circle_radius * circle_radius
+    return circle_area
+
+
+def calc_pizza_calories(pizza_diameter):
+    calories_per_square_inch = 16.7    # Regular crust pepperoni pizza
+
+    total_calories = calc_circle_area(pizza_diameter) * calories_per_square_inch
+    return total_calories
+
+
+print('12 inch pizza has {:.2f} calories.'.format(calc_pizza_calories(12.0)))
+print('14 inch pizza has {:.2f} calories.'.format(calc_pizza_calories(14.0)))
